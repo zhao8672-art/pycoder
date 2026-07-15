1f7888838f2ff3c499492f8c3552b9a9d035dfd4
zhao8672 <zhao8672@hotmail.com>
2026-07-15 09:45:03 +0800

    \ chore: 清理测试脚本\

--- scripts/_compile_check.py
+++
@@ -1,7 +0,0 @@
-import py_compile, sys
-try:
-    py_compile.compile("pycoder/server/services/execution_pipeline.py", doraise=True)
-    print("PIPELINE_OK")
-except py_compile.PyCompileError as e:
-    print(f"FAIL: {e}")
-    sys.exit(1)
